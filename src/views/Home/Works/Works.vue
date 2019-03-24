aa9eb09417a17e6dfac5923ae2f66bf91da7caaf
andrewbdesign <hello@andrewbdesign.com>
2019-03-25 07:12:58 +1100

    added works navigation urls

--- src/views/Home/Works/Works.vue
+++ src/views/Home/Works/Works.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>Selected works</h3>
+    <h3 class="work-title">Selected works</h3>
     <div class="works-section" v-if="works">
       <div
         class="work-tile"
@@ -9,12 +9,18 @@
         :key="work.id"
         :style="{ backgroundImage: 'url(' + work.image + ')' }"
         :image="work.image"
+        v-on:click="goToLink(work.url)"
       >
         <h3>{{ work.name }}</h3>
         <p>{{ work.description }}</p>
       </div>
     </div>
   </div>
+  <!-- <router-link to="/works/easy-bill-pay">Easy Bill Pay</router-link> - -->
+  <!-- <router-link to="/works/macquarie-telecom">Macquarie Telecom</router-link> - -->
+  <!-- <router-link to="/works/king-living">King Living</router-link> - -->
+  <!-- <router-link to="/works/redscope">Redscope</router-link> - -->
+  <!-- <router-link to="/works/edm-html5-motion">EDM HTML5 Motion</router-link> -->
 </template>
 
 <style lang="scss" scoped>
@@ -22,8 +28,9 @@ h3 {
   padding: 0;
   margin: 0;
 }
-h3, p {
-  opacity: 0;
+.work-title {
+  font-size: $l-size;
+  padding: 1em 0;
 }
 .works-section {
   // padding: $xl-size 0;
@@ -48,6 +55,11 @@ import TileMacq from '@/assets/work-tiles/WorkTile_MacT.jpg';
 import TileEDM from '@/assets/work-tiles/WorkTile_EDM.jpg';
 
 export default {
+  methods: {
+    goToLink(url) {
+      this.$router.push(`/works/${url}`);
+    },
+  },
   data() {
     return {
       works: [
@@ -56,30 +68,35 @@ export default {
           name: 'Easy Bill Pay',
           image: TileEBP,
           description: 'fill murray',
+          url: 'easy-bill-pay',
         },
         {
           id: 2,
           name: 'King Living',
           image: TileKL,
           description: 'fill murray',
+          url: 'king-living',
         },
         {
           id: 3,
           name: 'Redscope',
           image: TileRedscope,
           description: 'fill murray',
+          url: 'redscope',
         },
         {
           id: 4,
           name: 'Macquarie Telecom',
           image: TileMacq,
           description: 'fill murray',
+          url: 'macquarie-telecom',
         },
         {
           id: 5,
           name: 'EDM and banners',
           image: TileEDM,
           description: 'fill murray',
+          url: 'edm-html5-motion',
         },
       ],
     };
